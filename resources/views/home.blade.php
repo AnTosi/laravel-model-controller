57bfbfec43d9e18e71df2c0ddfb23147cdfc4926
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-11 16:56:26 +0100

    finish page, cards, css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>Ciao</h3>
+    <h1>These are our movies:</h1>
+    <div class="movies">
+        @forelse ($movies as $movie)
+            <div class="card">
+                <a href="">
+                    <h4>{{ $movie->title }}</h4>
+                    <img src="https://picsum.photos/200/" alt="">
+                    <p>{{ $movie->original_title }}</p>
+                    <p>Nationality: {{ $movie->nationality}}</p>
+                    <p>Release Date: {{ $movie->date }}</p>
+                    <p>Movie score: {{ $movie->vote }}</p>
+                </a>
+            </div>
+        @empty
+            <p>We are all out of movies :(</p>
+        @endforelse
+    </div>
 @endsection
